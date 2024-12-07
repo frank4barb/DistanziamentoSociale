c8700ebec118030e27e6a810085a08694dcd97b0
Francesco Barbera <59318545+frank4barb@users.noreply.github.com>
2024-12-07 18:11:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# DistanziamentoSociale
+# Distanziamento sociale
+
 Un'applicazione web che utilizza la videocamera del dispositivo per rilevare persone troppo vicine tra loro e avvisare con un segnale acustico. Questo strumento si basa sull'uso di modelli di machine learning per monitorare il distanziamento sociale.
+
+## Funzionalità principali
+
+- **Riconoscimento delle persone:** Utilizzo del modello pre-addestrato COCO-SSD per rilevare le persone nell'inquadratura.
+- **Avviso sonoro:** Genera un segnale acustico quando due o più persone sono troppo vicine tra loro.
+- **Visualizzazione in tempo reale:** Evidenzia le persone rilevate con riquadri, differenziando quelle che rispettano o violano il distanziamento sociale.
+
+## Installazione
+
+1. Scarica o clona il repository.
+2. Apri il file `index.html` in un browser che supporta l'accesso alla webcam (es. Chrome).
+3. Concedi il permesso al browser per accedere alla webcam.
+
+## Utilizzo
+
+1. Apri il file `index.html` in un browser.
+2. Fai clic sul pulsante "Enable Webcam" e consenti l'accesso alla webcam.
+3. L'applicazione inizierà a rilevare le persone e ti avviserà con un beep se sono troppo vicine.
+
+## Librerie utilizzate
+
+- [TensorFlow.js](https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js): Per il supporto al machine learning e al rilevamento degli oggetti.
+- [COCO-SSD](https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd): Modello di deep learning per il rilevamento degli oggetti.
+
+## File principali
+
+- **index.html:** La struttura della pagina web.
+- **script.js:** La logica principale dell'applicazione, inclusa l'integrazione del modello COCO-SSD.
+- **style.css:** Foglio di stile per la personalizzazione dell'aspetto (non fornito in questa versione, ma citato nell'HTML).
+
+## Note tecniche
+
+- Per il rilevamento viene utilizzata la funzione `model.detect(video)` del modello COCO-SSD.
+- La distanza tra le persone viene calcolata in base alle coordinate dei riquadri di rilevamento.
+- L'applicazione è progettata per browser moderni compatibili con l'API `getUserMedia`.
+
+## Limitazioni
+
+- Il rilevamento dipende dalla qualità della videocamera e dall'illuminazione.
+- Non garantisce una misurazione precisa delle distanze effettive (i calcoli si basano su pixel nell'immagine acquisita).
+
+## Autore
+
+Progetto ispirato e basato sulla guida ufficiale di TensorFlow.js per il rilevamento di oggetti: [Codelabs TensorFlow.js Object Detection](https://codelabs.developers.google.com/codelabs/tensorflowjs-object-detection#6).
+
+---
+Contribuisci o segnala problemi nel repository!
---
+++ script.js
@@ -0,0 +1,250 @@
+
+// https://codelabs.developers.google.com/codelabs/tensorflowjs-object-detection#6
+
+//----------------------------------------------------------------------
+// First, let's ensure we can access key parts of the page we will need to manipulate or access later on in our code:
+//----------------------------------------------------------------------
+
+
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+
+
+//----------------------------------------------------------------------
+// We can now add some assistive functions to check if the browser we are using supports accessing the webcam stream via getUserMedia:
+//----------------------------------------------------------------------
+
+
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+    
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
+}
+
+ 
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it to call enableCam function which we will 
+// define in the next step.
+enableWebcamButton.addEventListener('click', enableCam);
+
+
+//----------------------------------------------------------------------
+// Next we can fill out the code for the previously empty enableCam function we defined above.
+//----------------------------------------------------------------------
+
+
+// Enable the live webcam view and start classification.
+function enableCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
+  if (!model) {
+    return;
+  }
+  
+  // Hide the button once clicked.
+  event.target.classList.add('removed');  
+  
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream. (https://whatwebcando.today/camera-microphone.html)
+
+  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+
+  getUserMedia(constraints)
+    .then(function (stream) {
+      //var mediaControl document.querySelector(type);
+      
+      if ('srcObject' in video) {
+        video.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        video.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+      
+      //video.play();
+      video.addEventListener('loadeddata', predictWebcam);
+
+    })
+    .catch(function (err) {
+      alert('Webcam is not supported by your browser. Error: ' + err);
+    });
+
+
+}
+
+
+
+//----------------------------------------------------------------------
+// Finally lets just add some temporary code so we can test the webcam is working. The code below will pretend our model is loaded and enable the camera button so we can click it. We will replace all of this this code in the next step so be prepared to delete it again in a moment:
+//----------------------------------------------------------------------
+
+
+
+//xx//// Placeholder function for next step.
+//xx//function predictWebcam() {
+//xx//}
+
+//xx//// Pretend model has loaded so we can try out the webcam code.
+//xx//var model = true;
+//xx//demosSection.classList.remove('invisible');
+
+
+
+
+
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+
+
+
+
+
+
+//----------------------------------------------------------------------
+// We are now ready to load our COCO-SSD model. When it has finished initialising we enable the demo area and button on our web page (paste this code over the temporary code we added at the end of the last step).
+//----------------------------------------------------------------------
+
+
+// Store the resulting model in the global scope of our app.
+var model = undefined;
+
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment 
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
+
+
+
+
+//----------------------------------------------------------------------
+// The below code allows us to continuously grab a frame from the webcam stream when the browser is ready and pass it to our model to be classified. We then parse the results and draw a <p> tag at the coordinates that come back and set it's text to the name of what we found if it is over a certain level of confidence.
+// Now the really important call in this new code we added is model.detect(). All of our pre-made models for TensorFlow.js have a function like this (the name of which may change from model to model so check the docs for details) that actually performs the machine learning inference.
+// Inference is simply the act of taking some input and running it through the machine learning model (essentially a lot of mathematical operations), and then providing some results. With our pre-made models we return our predictions in the form of a JSON object so it is easy to use.
+//----------------------------------------------------------------------
+
+
+function beep() {
+  (new
+	Audio(
+	"data:audio/wav;base64,//uQRAAAAWMSLwUIYAAsYkXgoQwAEaYLWfkWgAI0wWs/ItAAAGDgYtAgAyN+QWaAAihwMWm4G8QQRDiMcCBcH3Cc+CDv/7xA4Tvh9Rz/y8QADBwMWgQAZG/ILNAARQ4GLTcDeIIIhxGOBAuD7hOfBB3/94gcJ3w+o5/5eIAIAAAVwWgQAVQ2ORaIQwEMAJiDg95G4nQL7mQVWI6GwRcfsZAcsKkJvxgxEjzFUgfHoSQ9Qq7KNwqHwuB13MA4a1q/DmBrHgPcmjiGoh//EwC5nGPEmS4RcfkVKOhJf+WOgoxJclFz3kgn//dBA+ya1GhurNn8zb//9NNutNuhz31f////9vt///z+IdAEAAAK4LQIAKobHItEIYCGAExBwe8jcToF9zIKrEdDYIuP2MgOWFSE34wYiR5iqQPj0JIeoVdlG4VD4XA67mAcNa1fhzA1jwHuTRxDUQ//iYBczjHiTJcIuPyKlHQkv/LHQUYkuSi57yQT//uggfZNajQ3Vmz+ Zt//+mm3Wm3Q576v////+32///5/EOgAAADVghQAAAAA//uQZAUAB1WI0PZugAAAAAoQwAAAEk3nRd2qAAAAACiDgAAAAAAABCqEEQRLCgwpBGMlJkIz8jKhGvj4k6jzRnqasNKIeoh5gI7BJaC1A1AoNBjJgbyApVS4IDlZgDU5WUAxEKDNmmALHzZp0Fkz1FMTmGFl1FMEyodIavcCAUHDWrKAIA4aa2oCgILEBupZgHvAhEBcZ6joQBxS76AgccrFlczBvKLC0QI2cBoCFvfTDAo7eoOQInqDPBtvrDEZBNYN5xwNwxQRfw8ZQ5wQVLvO8OYU+mHvFLlDh05Mdg7BT6YrRPpCBznMB2r//xKJjyyOh+cImr2/4doscwD6neZjuZR4AgAABYAAAABy1xcdQtxYBYYZdifkUDgzzXaXn98Z0oi9ILU5mBjFANmRwlVJ3/6jYDAmxaiDG3/6xjQQCCKkRb/6kg/wW+kSJ5//rLobkLSiKmqP/0ikJuDaSaSf/6JiLYLEYnW/+kXg1WRVJL/9EmQ1YZIsv/6Qzwy5qk7/+tEU0nkls3/zIUMPKNX/6yZLf+kFgAfgGyLFAUwY//uQZAUABcd5UiNPVXAAAApAAAAAE0VZQKw9ISAAACgAAAAAVQIygIElVrFkBS+Jhi+EAuu+lKAkYUEIsmEAEoMeDmCETMvfSHTGkF5RWH7kz/ESHWPAq/kcCRhqBtMdokPdM7vil7RG98A2sc7zO6ZvTdM7pmOUAZTnJW+NXxqmd41dqJ6mLTXxrPpnV8avaIf5SvL7pndPvPpndJR9Kuu8fePvuiuhorgWjp7Mf/PRjxcFCPDkW31srioCExivv9lcwKEaHsf/7ow2Fl1T/9RkXgEhYElAoCLFtMArxwivDJJ+bR1HTKJdlEoTELCIqgEwVGSQ+hIm0NbK8WXcTEI0UPoa2NbG4y2K00JEWbZavJXkYaqo9CRHS55FcZTjKEk3NKoCYUnSQ 0rWxrZbFKbKIhOKPZe1cJKzZSaQrIyULHDZmV5K4xySsDRKWOruanGtjLJXFEmwaIbDLX0hIPBUQPVFVkQkDoUNfSoDgQGKPekoxeGzA4DUvnn4bxzcZrtJyipKfPNy5w+9lnXwgqsiyHNeSVpemw4bWb9psYeq//uQZBoABQt4yMVxYAIAAAkQoAAAHvYpL5m6AAgAACXDAAAAD59jblTirQe9upFsmZbpMudy7Lz1X1DYsxOOSWpfPqNX2WqktK0DMvuGwlbNj44TleLPQ+Gsfb+GOWOKJoIrWb3cIMeeON6lz2umTqMXV8Mj30yWPpjoSa9ujK8SyeJP5y5mOW1D6hvLepeveEAEDo0mgCRClOEgANv3B9a6fikgUSu/DmAMATrGx7nng5p5iimPNZsfQLYB2sDLIkzRKZOHGAaUyDcpFBSLG9MCQALgAIgQs2YunOszLSAyQYPVC2YdGGeHD2dTdJk1pAHGAWDjnkcLKFymS3RQZTInzySoBwMG0QueC3gMsCEYxUqlrcxK6k1LQQcsmyYeQPdC2YfuGPASCBkcVMQQqpVJshui1tkXQJQV0OXGAZMXSOEEBRirXbVRQW7ugq7IM7rPWSZyDlM3IuNEkxzCOJ0ny2ThNkyRai1b6ev//3dzNGzNb//4uAvHT5sURcZCFcuKLhOFs8mLAAEAt4UWAAIABAAAAAB4qbHo0tIjVkUU//uQZAwABfSFz3ZqQAAAAAngwAAAE1HjMp2qAAAAACZDgAAAD5UkTE1UgZEUExqYynN1qZvqIOREEFmBcJQkwdxiFtw0qEOkGYfRDifBui9MQg4QAHAqWtAWHoCxu1Yf4VfWLPIM2mHDFsbQEVGwyqQoQcwnfHeIkNt9YnkiaS1oizycqJrx4KOQjahZxWbcZgztj2c49nKmkId44S71j0c8eV9yDK6uPRzx5X18eDvjvQ6yKo9ZSS6l//8elePK/Lf//IInrOF/FvDoADYAGBMGb7 FtErm5MXMlmPAJQVgWta7Zx2go+8xJ0UiCb8LHHdftWyLJE0QIAIsI+UbXu67dZMjmgDGCGl1H+vpF4NSDckSIkk7Vd+sxEhBQMRU8j/12UIRhzSaUdQ+rQU5kGeFxm+hb1oh6pWWmv3uvmReDl0UnvtapVaIzo1jZbf/pD6ElLqSX+rUmOQNpJFa/r+sa4e/pBlAABoAAAAA3CUgShLdGIxsY7AUABPRrgCABdDuQ5GC7DqPQCgbbJUAoRSUj+NIEig0YfyWUho1VBBBA//uQZB4ABZx5zfMakeAAAAmwAAAAF5F3P0w9GtAAACfAAAAAwLhMDmAYWMgVEG1U0FIGCBgXBXAtfMH10000EEEEEECUBYln03TTTdNBDZopopYvrTTdNa325mImNg3TTPV9q3pmY0xoO6bv3r00y+IDGid/9aaaZTGMuj9mpu9Mpio1dXrr5HERTZSmqU36A3CumzN/9Robv/Xx4v9ijkSRSNLQhAWumap82WRSBUqXStV/YcS+XVLnSS+WLDroqArFkMEsAS+eWmrUzrO0oEmE40RlMZ5+ODIkAyKAGUwZ3mVKmcamcJnMW26MRPgUw6j+LkhyHGVGYjSUUKNpuJUQoOIAyDvEyG8S5yfK6dhZc0Tx1KI/gviKL6qvvFs1+bWtaz58uUNnryq6kt5RzOCkPWlVqVX2a/EEBUdU1KrXLf40GoiiFXK///qpoiDXrOgqDR38JB0bw7SoL+ZB9o1RCkQjQ2CBYZKd/+VJxZRRZlqSkKiws0WFxUyCwsKiMy7hUVFhIaCrNQsKkTIsLivwKKigsj8XYlwt/WKi2N4d//uQRCSAAjURNIHpMZBGYiaQPSYyAAABLAAAAAAAACWAAAAApUF/Mg+0aohSIRobBAsMlO//Kk4soosy1JSFRYWaLC4qZBYWFRGZdwqKiwkNBVmoWFSJkWFxX4FFRQWR+LsS4W/rFRb//////////////////////////// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////VEFHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAU291bmRib3kuZGUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMjAwNGh0dHA6Ly93d3cuc291bmRib3kuZGUAAAAAAAAAACU="
+	)).play();
+}
+
+var children = [];
+
+function predictWebcam() {
+
+  //console.log(video.getBoundingClientRect().width+"-"+video.videoWidth);
+  const wRatio = video.getBoundingClientRect().width / video.videoWidth; //const hRatio = wRatio;
+  const hRatio = video.getBoundingClientRect().height / video.videoHeight;
+
+  // Now let's start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+    
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].class == 'person' && predictions[n].score > 0.66) {
+      
+        var highlighter_type = 'highlighter';
+        
+      	//--- VERIFICO SE LA PERSONE E' VICINA AD ALTRE
+        var x = predictions[n].bbox[0] + predictions[n].bbox[2] / 2, y = predictions[n].bbox[1] + predictions[n].bbox[3] / 2, z = predictions[n].bbox[2];
+        for (let m = 0; m < predictions.length; m++) {
+          // If we are over 66% sure we are sure we classified it right, draw it!
+          if (m!=n && predictions[m].class == 'person' && predictions[m].score > 0.66) {
+            var x1 = predictions[m].bbox[0] + predictions[m].bbox[2] / 2, y1 = predictions[m].bbox[1] + predictions[m].bbox[3] / 2, z1 = predictions[m].bbox[2];
+            var dist = Math.floor(Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1))) ; // distanza in pixel
+            if (dist < z && dist > (z / 100)) { highlighter_type = 'highlighter_near'; } // distanza troppo corta
+      	  }
+      	}
+      	if (highlighter_type == 'highlighter_near') beep();
+      	//---
+      
+      
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + '% confidence.';
+        p.style = 'margin-left: ' + (predictions[n].bbox[0] * wRatio) + 'px; margin-top: '
+            + ((predictions[n].bbox[1] * hRatio) - 10) + 'px; width: ' 
+            + ((predictions[n].bbox[2] * wRatio) - 10) + 'px; top: 0; left: 0;';
+
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', highlighter_type);
+        highlighter.style = 'left: ' + (predictions[n].bbox[0] * wRatio) + 'px; top: '
+            + (predictions[n].bbox[1] * hRatio) + 'px; width: ' 
+            + (predictions[n].bbox[2] * wRatio) + 'px; height: '
+            + (predictions[n].bbox[3] * hRatio) + 'px;';
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
